e58fc270b5d2356c39fe5326608341c3435ee97f
gar1 <garvanpax@aol.com>
2014-07-07 12:15:38 -0400

    question123RetryRetry
    I got question #2 working now.

---
+++ ConsoleGarnett/ConsoleGarnett2/Book.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleGarnett2
+{
+    class Book
+    {
+        private string name;
+        private decimal price;
+
+        public Book(string name, decimal price)
+        {
+            this.Name = name;
+            this.Price = price;
+        }
+
+        public Book()
+        {
+            name = "";
+            price = 0M;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public decimal Price { get; set; }
+
+        public int Compare(Book OtherBook)
+        {
+            if (this.Price > OtherBook.Price)
+            {
+                return 1;
+            }
+            else if (this.Price < OtherBook.Price)
+            {
+                return -1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
--- ConsoleGarnett/ConsoleGarnett2/Program.cs
+++ ConsoleGarnett/ConsoleGarnett2/Program.cs
@@ -4,52 +4,42 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//--------------------------------------------------------------------------
+// With a little help from a class mate, I was able to get this one working.
+//--------------------------------------------------------------------------
+
 namespace ConsoleGarnett2
 {
-    //----------------------------------------------------------------------------------------|
-    // I went home and worked on quiz problem #2.  I could NOT figure this one out on my own. | 
-    //----------------------------------------------------------------------------------------|
-    class Book
-    {
-        private string name;
-        private decimal price;
-
-        public Book(string name, decimal price);
-
-        public string Name
-        {
-            get {return name;}
-            set {name = value;}
-        }
-
-        public decimal Price
-        {
-            get {return price;}
-            set {price = value;}
-        }
-
-        public int Compare()
-        {
-
-            return 1;
-
-        }
-
-    }
     class Program
     {
-
         static void Main(string[] args)
         {
+            // Read the name and price for all three into the class named Book.
+            Book book1 = new Book {Name = "Gone With The Wind", Price = 89.99M};
+            Book book2 = new Book("The Wizard Of Oz", 45.00M);
+            Book book3 = new Book();
+            book3.Name = ("Rear Window");
+            book3.Price = 24.50M;
 
-            Book book1 = new Book(name, price){"Gone With The Wind", 89.99};
-            Book book2 = new Book("The Wizard Of Oz", 45.00);
+            // Display the name and price for all three.
+            Console.WriteLine(book1.Name + " " + book1.Price);
+            Console.WriteLine(book2.Name + " " + book2.Price);
+            Console.WriteLine(book3.Name + " " + book3.Price);
+            Console.WriteLine();
 
-            Console.WriteLine(book1.name + " " + book1.price);
-            Console.WriteLine(book2.name + " " + book2.price);
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
 
+            // Use the Compare method in the Book class to return a value.
+            Book book4 = new Book();
+            Book book5 = new Book();
+            Console.WriteLine("Enter the first book price:");
+            book4.Price = decimal.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second book price:");
+            book5.Price = decimal.Parse(Console.ReadLine());
 
-
+            Console.WriteLine(book4.Compare(book5));
+            Console.ReadLine();
         }
     }
 }
